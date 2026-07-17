c8c1b479c977b3b13688733322f02e3e393e7bd3
Hunter H <hunterh.online@gmail.com>
2026-07-17 14:08:05 -0400

    RenderGallery: drop DicyaninMetaballs & DicyaninSpatialUI references
    Those packages now live in standalone repos:
      https://github.com/hunterh37/DicyaninMetaballs
      https://github.com/hunterh37/DicyaninSpatialUI

--- RenderGallery/Package.swift
+++ RenderGallery/Package.swift
@@ -8,28 +8,13 @@ let package = Package(
     ],
     dependencies: [
         .package(path: "../DicyaninHumanoidMesh"),
-        .package(path: "../DicyaninMetaballs"),
-        .package(path: "../DicyaninSpatialUI"),
         .package(path: "../DicyaninVirtualJoystick")
     ],
     targets: [
         .executableTarget(
             name: "RenderGallery",
             dependencies: [
-                .product(name: "DicyaninHumanoidMesh", package: "DicyaninHumanoidMesh"),
-                .product(name: "DicyaninMetaballs", package: "DicyaninMetaballs")
-            ]
-        ),
-        .executableTarget(
-            name: "SpatialUIGallery",
-            dependencies: [
-                .product(name: "DicyaninSpatialUI", package: "DicyaninSpatialUI")
-            ]
-        ),
-        .executableTarget(
-            name: "MetaballGallery",
-            dependencies: [
-                .product(name: "DicyaninMetaballs", package: "DicyaninMetaballs")
+                .product(name: "DicyaninHumanoidMesh", package: "DicyaninHumanoidMesh")
             ]
         ),
         .executableTarget(
